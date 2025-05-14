bd2a0ed85ef10447ed32cef9635ce631b18d79af
Aadish Jain <aadishhere@gmail.com>
2025-05-14 11:15:12 +0530

    Initial Commit

---
+++ bottomBarWithPages/bottomBarWithPagesApp.swift
@@ -0,0 +1,17 @@
+//
+//  bottomBarWithPagesApp.swift
+//  bottomBarWithPages
+//
+//  Created by Aadish Jain on 14/05/25.
+//
+
+import SwiftUI
+
+@main
+struct bottomBarWithPagesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
